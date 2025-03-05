c480e36be54f0ff54c481ecca16da859d585372d
Deickar <deickar@outlook.com>
2025-03-05 17:41:20 -0600

    se agrego ls funcion recursividad

--- recursividad/Source/main.cpp
+++ recursividad/Source/main.cpp
@@ -1,7 +1,24 @@
-
+#include <iostream>
+ 
+int factorial(int n){
+  if(n==0){
+    return 1;
+  }
+  return n * factorial(n-1);
+}
 
 int main(int argc, const char* argv[])
 {
-	std::cout << "Hello from Magnet 🧲 \n";
+  int numero;
+  std::cout<<"Ingresa un numero del 1 al 5 \n";
+  std::cin>> numero;
+  if(numero < 0){
+    std::cout<<"Error el factorial no puede ser defini como negativo\n";
+  }
+  else{
+    std::cout <<"El factorial de " <<numero << " es " << factorial(numero)<<std::endl;
+  }
+
+
 	return 0;
 }
